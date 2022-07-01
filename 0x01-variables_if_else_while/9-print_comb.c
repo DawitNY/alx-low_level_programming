658d1822d551a1719f94b58cfbf6ee832c19ca9c
Dawit <dndnegussie@gmail.com>
2022-07-01 13:32:15 -0700

    11

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>                                                                                                                             
+#include <time.h>                                                                                                                               
+#include <stdio.h> 
+
+/**                                                                      
+ **main -> assgin random number each time program is run                 
+ **based a condition                                                     
+ **Return: 0                                                             
+ */
+
+                                                                   
+int main(void){                                                                                                                                 
+	int c;                                                                       
+
+	for (c='0'; c<= '8'; ++c)
+	{                                                   	 
+			putchar (c);
+				putchar (' ');
+					putchar (',');
+	}
+	puts (" 9$");                                                                                                                                 
+	return 0;                                                                                                                                       
+}   
+
