be71935f16b5f37694dbf2ec39f95f2e06aa1168
Dawit <dndnegussie@gmail.com>
2022-07-01 14:06:31 -0700

    tt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>                                                                                                                             
+#include <time.h>                                                                                                                               
+#include <stdio.h> 
+
+/**                                                                      
+ **main -> assgin random number each time program is run                 
+ **based a condition                                                     
+ **Return: 0                                                             
+ */
+
+                                                                   
+int main(void){                                                                                                                                 
+	int c;                                                                       
+
+
+
+	for (c='a'; c<= 'z'; ++c)
+	{                                                   	 
+			if (c!='q' && c!='e')
+				    {
+					        putchar (c);
+						    }
+			  
+	}
+	                                                
+	return 0;                                                                                                                                       
+} 
