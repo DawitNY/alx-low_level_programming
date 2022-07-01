0a83b0f839f0395bf01fb6216af465c7593b29b1
Dawit <dndnegussie@gmail.com>
2022-07-01 12:20:46 -0700

    2

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>                                                                                                                             
 #include <time.h>                                                                                                                               
 #include <stdio.h>                                                                                                                              
-/**                                                                     **main -> run a loop                                                   **use loop to output alphabet                                          **Return: 0                                                            **/ 
+/**                                                                     **main -> run a loop                                                   **use loop to output hexadecimal                                       **Return: 0                                                           **/ 
                                                                                                                                                 
 int main(void){                                                              
 	int c;                                                                              
-	for (c='1'; c<= '9'; ++c)                                                   	putchar (c);  
+	for (c='0'; c<= '9'; ++c)                                                   	putchar (c);  
 	         
 	putchar ('\n');                                                                                                                                 
 	return 0;                                                                                                                                       
