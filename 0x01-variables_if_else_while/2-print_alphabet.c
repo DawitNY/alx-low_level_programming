a36cbeb42c0d56f1440431e33904e11a3628ff45
Dawit <dndnegussie@gmail.com>
2022-07-01 11:46:51 -0700

    123

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**                                                                        
+ **main -> run a loop                         
+ **use loop to output alphabet                                          
+ **Return: 0                                                                
+ **/ 
+
+int main(void){
+int c;
+for (c = 'a'; c <= 'z'; ++c)
+	putchar (c);
+putchar ('\n');
+return 0;
+}                                                                         
+                                                                            
+
