65697959263aaa403f8f60a25c0c8417e988d1a3
Dawit <dndnegussie@gmail.com>
2022-07-01 12:42:57 -0700

    1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>                                                      
+                                                                         
+#include <time.h>                                                        
+                                                                         
+#include <stdio.h>                                                       
+                                                                         
+/**                                                                      
+ **main -> assgin random number each time program is run                 
+ **based a condition                                                     
+ **Return: 0                                                             
+ **/                                                                     
+                                                                         
+                                                                         
+int main(void){                                                         
+int c;                                                          
+for (c='0'; c<= '9'; ++c)                                       
+	putchar (c);                                         
+putchar ('\n');                                                
+return 0;                                                        
+					                                                                         
+					                                                                         
+} 
