62eb2e081737cdc1bef5aef3c9dd69d611664d24
Dawit <dndnegussie@gmail.com>
2022-07-05 14:37:37 -0700

    1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs -. return absoulte value of a number
+ * @n: parameter
+ * return: absolute of a number
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		n = -n;
+	return (n);
+}
