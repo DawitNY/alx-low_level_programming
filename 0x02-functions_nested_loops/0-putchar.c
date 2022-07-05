0375425142b893c21b8af6501646c3f45d2ca63d
Dawit <dndnegussie@gmail.com>
2022-07-05 04:50:28 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
-/**              
- **main -> assgin random number each time program is run   
- **based a condition             
- **Return: 0       
+/**
+ **main -> assgin random number each time program is run
+ **based a condition
+ **Return: 0
 */
 
-int main() 
+int main(void) 
 {
 	_putchar('_');
-	_putchar('p'); 
-	_putchar('u'); 
-	_putchar('t'); 
-	_putchar('c'); 
-	_putchar('h'); 
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-        _putchar('r');
-	_putchar('\n');        
-return 0;
+	_putchar('r');
+	_putchar('\n');
+return (0);
 }
