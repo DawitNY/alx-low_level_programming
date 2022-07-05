911e0005685594c3e80bc6241ff373e1be4bc603
Dawit <dndnegussie@gmail.com>
2022-07-05 07:49:53 -0700

    1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print sign of number
+ * @i number variable 
+ *
+ * returns 1,0,-1
+ */
+int print_sign(int n)
+{
+	if (i > 0)
+	{
+		_putchar('+');
+		return (1)
+	}
+	else if (i==0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1)
+	}
+}	
