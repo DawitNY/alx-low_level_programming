7cef5de38d404f911a86708bb6441a4148392227
Dawit <dndnegussie@gmail.com>
2022-07-05 03:13:53 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**                                                                     **main -> assgin random number each time program is run                **based a condition             
+ **Return: 0       
+*/
+
+int main() {
+printf("_putchar\n");
+return 0;
+}
