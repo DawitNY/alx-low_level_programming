386abfe4c9cf5a167475346fc5c9f0bb48ed6329
Dawit <dndnegussie@gmail.com>
2022-07-06 03:00:40 -0700

    1

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include  "main.h"
+
+/**
+ * times_table - times table 1-9
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, count;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putcchar(48);
+		for (j = 1; j <= 9; j++)
+		{
+			count = i*j;
+			_putchar(44);
+			_putchar(32);
+			if (count <= 9)
+			{
+				_putchar(44);
+				_putchar(count + 48);
+			}
+			else
+			{
+				_putchar((count / 10) + 48);
+				_putchar((count % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
+				
