d1a76e7b32ba159e64f1e9fc16cc06371e676dec
Dawit <dndnegussie@gmail.com>
2022-07-06 02:36:21 -0700

    1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,14 +17,14 @@ void jack_bauer(void)
 			{
 				for (d = 48; d <= 57; d++)
 				{
-					if (a >= 50 && s >= 52)
+					if (a >= 50 && b >= 52)
 						break;
 					_putchar(a);
 					_putchar(b);
 					_putchar(':');
 					_putchar(c);
 					_putchar(d);
-					-putchar('\n');
+					_putchar('\n');
 				}
 			}
 		}
