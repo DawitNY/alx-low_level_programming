9986f1911c87a2587f1dff975bd724ab3cd84655
Dawit <dndnegussie@gmail.com>
2022-07-07 06:05:53 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,9 +2,9 @@
 
 /**
  * main - casue infinte loop
- *
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop inccoming :(\n");
 
 	i = 0;
-
-	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
-
+	/**
+	*while (i < 10)
+	*{
+	*putchar(i);
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
