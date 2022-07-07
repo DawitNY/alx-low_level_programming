8f8f11d2dfadd68d2a40fa40738b2fdad4a7509a
Dawit <dndnegussie@gmail.com>
2022-07-07 01:57:42 -0700

    1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - casue infinte loop
+ * Retun: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop inccoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
