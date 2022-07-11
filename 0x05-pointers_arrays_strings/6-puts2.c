80f72df8a8da187373a03b2b260ed8662e7f221d
Dawit <dndnegussie@gmail.com>
2022-07-11 04:37:37 -0700

    1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - print str form first char
+ * @str: string
+ * Retrun: void
+ */
+void puts2(char *str)
+{
+	int tint = 0;
+
+	while (tint >= 0)
+	{
+		if (str[tint] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (tint % 2 ==  0)
+			_putchar(str[tint])
+				tint ++;
+	}
+}
