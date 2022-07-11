3e5d0c9fea71f858b07f12a8874c8b64d8db4c05
Dawit <dndnegussie@gmail.com>
2022-07-11 05:08:30 -0700

    1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies
+ * @dest: end
+ * @src: source
+ * Return: pointer to dest
+ */
+char *strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0)
+			break;
+		count++;
+	}
+	return (dest)
+}
