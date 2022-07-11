e61a6c1f6b53b8525dd194ef7069a2723ec8af55
Dawit <dndnegussie@gmail.com>
2022-07-11 04:28:32 -0700

    1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - string reversely
+ * @s: string 
+ * Return: void
+ */
+void rev_string(char *s)	
+{
+	int revc = 0;
+	int i, j;
+	char temp, *str;
+
+	while (revc >= 0)
+	{
+		if (s[revc] == '\0')
+			break;
+		revc++;
+	}
+	str = s;
+
+	for (i = 0; i < (revc -  1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str * j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j-1)) = temp;
+		}
+	}
+}
