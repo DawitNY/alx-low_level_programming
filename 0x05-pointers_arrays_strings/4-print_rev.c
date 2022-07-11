44d4b45cc206642f7cdf304a362a09af62747fb7
Dawit <dndnegussie@gmail.com>
2022-07-11 04:17:27 -0700

    1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print str rev
+ * @s: str
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int revc = 0;
+
+	while (revc >= 0)
+	{
+		if (s[revc] == '\0')
+			break;
+		revc++;
+	}
+
+	for  (revc--, revc >= 0; revc--)
+		_putchar(s[revc]);
+	_putchar('\n');
+}
