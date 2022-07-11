9339b6c49cf866e12800aef103a6b40d9f8dc65f
Dawit <dndnegussie@gmail.com>
2022-07-11 03:56:37 -0700

    1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length
+ * @s: string
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '\0')
+		length ++;
+	return (length);
+}
