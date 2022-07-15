4e52f5b7f09167f2049f194cc6643746efc12d26
Dawit <dndnegussie@gmail.com>
2022-07-14 17:22:47 -0700

    0

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,30 +1,12 @@
-Skip to content
-Samuel-Igwudu
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-More
-alx-low_level_programming/0x06-pointers_arrays_strings/4-rev_array.c
-@Samuel-Igwudu
-Samuel-Igwudu a function that reverses the content of an array of integers
- History
-  1 contributor
-  22 lines (20 sloc)  335 Bytes
 #include "main.h"
 
-  /**
-   *  * reverse_array - reverse the content of an array of int
-   *   * @a: array
-   *    * @n: elements of array
-   *     * Return: void
-   *      */
-  void reverse_array(int *a, int n)
+/**
+ *  * reverse_array - reverse the content of an array of int
+ *   * @a: array
+ *    * @n: elements of array
+ *     * Return: void
+ *      */
+void reverse_array(int *a, int n)
 {
 		int i, j, k;
 
@@ -36,5 +18,5 @@ Samuel-Igwudu a function that reverses the content of an array of integers
 																		*(a + j) = *(a + (j - 1));
 																					*(a + (j - 1)) = k;
 																							}
-					}
+									}
 }
