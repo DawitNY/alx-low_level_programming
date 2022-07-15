3f35de2a08d822da094ce23e6479acefb8da495e
Dawit <dndnegussie@gmail.com>
2022-07-14 17:12:16 -0700

    1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,17 +6,17 @@
  * @s2: str
  * Return: 0
  */
-int _strcom(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 	int j = 0;
 
 	while (j == 0)
 	{
-		if ((*s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
 		j = *(s1 + i) - *(s2 + i);
-		i ++;
+		i++;
 	}
 	return (j);
 }
