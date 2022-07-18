ec29f1830ac9e025e5a11ceb1495ab506f11b69f
Dawit <dndnegussie@gmail.com>
2022-07-18 07:19:36 -0700

    1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - length of a prefix 
+ * @s: segment of byte
+ * @accept: bytes
+ * Return: no of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+	}
+	return (c);
+}
