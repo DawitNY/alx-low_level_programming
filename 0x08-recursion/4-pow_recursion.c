3853b375740d07edbbac4f08f029ed1b13294487
Dawit <dndnegussie@gmail.com>
2022-07-20 12:24:30 -0700

    1

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns x raised to y
+ * @x: base
+ * @y: exponent
+ * Return: x raised to y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		retunr (x * _pow_recursion(x, y - 1));
+}
