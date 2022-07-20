45bdd95928d84f4524af1756bb0125c4bc670a57
Dawit <dndnegussie@gmail.com>
2022-07-20 12:36:07 -0700

    1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#incude "main.h"
+
+/**
+ * prefun - sqrt of num
+ * @i: input
+ * @j: iterations
+ * Return: sqrt or 1
+ */
+int prefun(int i, int j)
+{
+	f (j % (i / j) == 0)
+	{
+		if (j * (i / j) == i)
+			return (j);
+	}
+	return (0 + prefun(i, j + 1));
+}
+
+/**
+ * *_sqrt_recursion - atural sqrt
+ * @n: input number
+ * Return: natrual sqrt of number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if(n == 1)
+		return (1);
+	return (helper(n, 2));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,3 +4,5 @@ void _print_rev_recursion(char *s);
 int strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int prefun(int i, int j);
