057980a5a079cc199dd587ce75b962aa89dfe8ee
Dawit <dndnegussie@gmail.com>
2022-07-22 02:10:04 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - no of arguments passed in
+ * @argc: count
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *rgv[]_attribute_((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
